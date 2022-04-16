3cd8af62fc4e7693b67adc3c79111ef535faf660
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-04-16 17:19:46 +0100

    finalizando o exercio, inserindo dados do formulario em uma Api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,96 @@
 import React from "react"; 
 
+
+const formFields = [
+  {
+    id:'nome',
+    label:'Nome',
+    type:'text'
+  },
+  {
+    id:'email',
+    label:'Email',
+    type:'email'
+  },
+
+  {
+    id:'senha',
+    label:'Senha',
+    type:'password'
+  },
+
+
+  {
+    id:'cep',
+    label:'Cep',
+    type:'text'
+  },
+  
+  {
+    id:'rua',
+    label:'Rua',
+    type:'text'
+  },
+
+  {
+    id:'numero',
+    label:'Numero',
+    type:'text'
+  },
+
+  {
+    id:'bairro',
+    label:'Bairro',
+    type:'text'
+  },
+
+  {
+    id:'cidade',
+    label:'Cidade',
+    type:'text'
+  },
+
+  {
+    id:'estado',
+    label:'Estado',
+    type:'text'
+  },
+
+]
+
+
 function App() {
 
   /*const [nome, setNome] = React.useState(' ');
   const [email, setEmail] = React.useState(' ');*/
 
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: ''
-  });
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field)=>{
+      return{
+        ...acc,
+        [field.id]:'',
+      }
+    },{}),
+ 
+  );
+
+  const [response, setResponse] = React.useState(null);
 
   function handleSubmit(event){
 
     event.preventDefault();
 
+    fetch("https://ranekapi.origamid.dev/json/api/usuario",{
+      method: 'POST',
+
+      headers:{
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify(form),
+
+    }).then(response => setResponse(response))
+    .catch(error => console.log(error))
   
   }
 
@@ -27,15 +104,15 @@ function App() {
   return (
  
     <form onSubmit={handleSubmit}>
-      <label htmlFor="nome">nome</label><br></br>
-      <input id="nome" type={'text'} value={form.nome} name="nome" onChange={handleChange}></input>
-      <br></br>
-      <label htmlFor="email">email</label><br></br>
-      <input id="email" type={'email'} value={form.email} name="email" onChange={handleChange}></input>
+  
+      {formFields.map(({id, label, type})=>
+      <div key={id}>
+        <label htmlFor={id}>{label}</label><br></br>
+        <input id={id} type={type} value={form[id]} onChange={handleChange}></input>
+      </div>)}
 
-
-      <p>{form.nome}</p>
-      <p>{form.email}</p>
+       {response && response.ok && <p>Formulario enviado</p>}
+ 
 
       <button>Enviar</button>
     </form>
